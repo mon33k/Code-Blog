2de030317dcd2247f989041ec8911a684e955caa
mon33k <moniquemojica@ac.c4q.nyc>
2020-11-24 21:35:43 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+
+  ],
 }
