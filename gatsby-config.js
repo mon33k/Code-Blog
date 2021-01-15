d70d89204885f1ae631366329f4bf8d52cc8d108
mon33k <moniquemojica@pursuit.org>
2021-01-14 22:14:48 -0500

    added 3rd day

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,16 +6,16 @@ module.exports = {
         menuLinks: [
             {
                 name: 'Day 1',
-                link: '/posts/2020/2020-12-31-first-post/'
+                link: '/posts/2021/2021-01-12-first-post/'
             },
-            // {
-            //     name: 'Day 2',
-            //     link: '/posts/2020/2020-09-05-second-post/'
-            // },
-            // {
-            //     name: 'Day 3',
-            //     link: '/posts/2020/2020-09-10-third-post/'
-            // }
+            {
+                name: 'Day 2',
+                link: '/posts/2021/2021-01-13-second-post/'
+            },
+            {
+                name: 'Day 3',
+                link: '/posts/2021/2021-01-14-third-post/'
+            }
         ]
 
     },
@@ -42,8 +42,26 @@ module.exports = {
             options: {
               pathToConfigModule: `src/utils/typography`,
             },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+              plugins: [
+                `gatsby-remark-relative-images`,
+                `gatsby-remark-gifs`,
+                {
+                  resolve: `gatsby-remark-images`,
+                  options: {
+                    maxWidth: 800,
+                    linkImagesToOriginal: false,
+                    sizeByPixelDensity: true,
+                    showCaptions: true
+                  }
+                },
+              ]
+            }
           },
-        `gatsby-transformer-remark`,
+        `gatsby-transformer-sharp`,
         `gatsby-plugin-sass`
     ],
 }
